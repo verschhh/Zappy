66ca6cf076f228574b24620c3423a48b21f781c5
Paul THIEBAULT <paul.thiebault@epitech.eu>
2023-05-22 16:40:06 +0200

    add: gui class

--- gui/includes/gui.hpp
+++ gui/includes/gui.hpp
@@ -8,3 +8,41 @@
 #pragma once
 
 #include <iostream>
+
+namespace zappy {
+    class Gui {
+        public:
+            Gui(int port) {
+                _port = port;
+                _machine = "localhost";
+            }
+
+            Gui(int port, std::string machine) {
+                _port = port;
+                _machine = machine;
+            }
+
+            ~Gui() {
+
+            }
+
+            //* Getters
+            int getPort(void) {
+                return (_port);
+            }
+            std::string getMachine(void) {
+                return (_machine);
+            }
+
+            //* Setters
+            void setPort(int port) {
+                _port = port;
+            }
+            void setMachine(std::string machine) {
+                _machine = machine;
+            }
+        private:
+            int _port;
+            std::string _machine;
+    };
+}
