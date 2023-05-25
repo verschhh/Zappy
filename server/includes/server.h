529f6427cab63ffa8b15f5f11a7ac1555b16ee38
Léonard Verschuere <leonard.verschuere@epitech.eu>
2023-05-25 17:23:37 +0200

    add: setting up the server, now the server accept an 'infinite' number of incoming connection

--- server/includes/server.h
+++ server/includes/server.h
@@ -9,12 +9,25 @@
     #define _SERVER_H_
 
     #include <stdio.h>
-    #include <string.h>
     #include <stdlib.h>
     #include <unistd.h>
     #include <getopt.h>
+    #include <sys/socket.h>
+    #include <sys/types.h>
+    #include <sys/select.h>
+    #include <arpa/inet.h>
+    #include <netinet/in.h>
+    #include <error.h>
+    #include <string.h>
 
 static const int MAX_NAMES = 10;
+
+enum orientation {
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST
+};
 
 typedef struct args_s {
     int port;
@@ -26,8 +39,45 @@ typedef struct args_s {
     int namesCount;
 } args_t;
 
-//* Parsing
+typedef struct inv_s {
+    int food;
+    int linemate;
+    int deraumere;
+    int sibur;
+    int mendiane;
+
+    int phiras;
+    int thystame;
+} inv_t;
+
+typedef struct client_s {
+    char *team_name;
+    int x;
+    int y;
+    int level;
+    inv_t *inventory;
+    int orientation;
+    int sockfd;
+    struct sockaddr_in addr;
+    socklen_t addrlen;
+    struct client_s *next;
+} client_t;
+
+typedef struct serv_s {
+    int sockfd;
+    int portno;
+    int max_sd;
+    struct sockaddr_in addr;
+    pid_t pid;
+    int global_uid;
+    client_t *clients;
+} serv_t;
+
 int parse_arguments(int ac, char **av, args_t *args);
+serv_t *serv_ctor(args_t *arg);
+client_t *root_client_ctor(void);
+client_t *client_ctor(client_t *root);
+client_t *pop_client(client_t *client);
 
 //* Tools
 void print_and_exit(char *str);
--- server/src/main.c
+++ server/src/main.c
@@ -7,6 +7,12 @@
 
 #include "../includes/server.h"
 
+void err_n_die(const char *string)
+{
+    printf("%s\n", string);
+    exit (84);
+}
+
 void usage(void)
 {
     printf("USAGE: ./zappy_server -p port -x width -y height -n name1 name2 ... -c clientsNb\n");
@@ -19,17 +25,50 @@ void usage(void)
     printf("\tfreq\tis the reciprocal of time unit for execution of actions\n");
 }
 
-int main(int ac, char **av)
+int retrieve_command(int sockfd, fd_set *readfds, serv_t *serv)
 {
-    if (ac == 2 && strcmp(av[1], "-help") == 0) {
-        usage();
-        return 0;
-    }
+    // char buffer[1024] = {0};
+    // int bytes_read = recv(sockfd, buffer, sizeof(buffer), 0);
+    // if (bytes_read <= 0) {
+    //     close(sockfd);
+    //     FD_CLR(sockfd, readfds);
+    // } else {
+    //     message_t message = parse_message(buffer);
+    //     handle_command(message, serv, sockfd);
+    // }
+    return 0;
+}
 
-    args_t *args = malloc(sizeof(args_t));
+void accept_new_client(fd_set *readfds, serv_t *serv)
+{
+    client_t *client = client_ctor(serv->clients);
+    client->sockfd = accept(serv->sockfd,
+        (struct sockaddr *)&client->addr, &client->addrlen);
+    inet_ntoa(client->addr.sin_addr);
+    int temp = ntohs(client->addr.sin_port);
+    (void)temp;
+    FD_SET(client->sockfd, readfds);
+    if (client->sockfd > serv->max_sd)
+        serv->max_sd = client->sockfd;
+}
 
-    if (parse_arguments(ac, av, args) == 84) {
-        return 84;
+int start_server(fd_set *readfds, serv_t *serv)
+{
+    fd_set tmpfds;
+    tmpfds = *readfds;
+
+    if (select(serv->max_sd + 1, &tmpfds, NULL, NULL, NULL) < 0)
+        err_n_die("ERROR on select");
+
+    for (int i = 0; i <= serv->max_sd; i++) {
+        if (!FD_ISSET(i, &tmpfds)) {
+            continue;
+        }
+        if (i == serv->sockfd) {
+            accept_new_client(readfds, serv);
+        } else {
+            retrieve_command(i, readfds, serv);
+        }
     }
 
     printf("port: %d, width: %d, height: %d, clientsNb: %d, freq: %.2f\n",
@@ -40,4 +79,35 @@ int main(int ac, char **av)
     for (int i = 0; i < args->namesCount; i++) {
         printf("Name %d: %s\n", i + 1, args->names[i]);
     }
+}
+
+int body(args_t *args)
+{
+    serv_t *serv = serv_ctor(args);
+    fd_set readfds;
+    if (serv == NULL) {
+        free(args);
+        return 84;
+    }
+    FD_SET(serv->sockfd, &readfds);
+    serv->max_sd = serv->sockfd;
+    while (1)
+        start_server(&readfds, serv);
+    free(args);
+    return 0;
+}
+
+int main(int ac, char **av)
+{
+    args_t *args = malloc(sizeof(args_t));
+    if (ac == 2 && strcmp(av[1], "-help") == 0) {
+        usage();
+        free(args);
+        return 0;
+    }
+    if (parse_arguments(ac, av, args) == 84) {
+        free(args);
+        return 84;
+    }
+    body(args);
 }
