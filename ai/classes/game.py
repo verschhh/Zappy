30a7cb665c3d1211d99095b9aa188b88420fc315
durbaon <nathanpaillet@gmail.com>
2023-05-31 16:30:51 +0200

    [ADD] player class

---
+++ ai/classes/game.py
@@ -0,0 +1,14 @@
+##
+## EPITECH PROJECT, 2023
+## Zappy
+## File description:
+## game
+##
+
+class Game:
+    def __init__(self):
+        self.map_size_x = 0
+        self.map_size_y = 0
+        self.map = []
+        self.players = []
+        self.eggs = []
