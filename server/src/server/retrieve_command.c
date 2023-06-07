3e55e8141420d43ac77b5c3637a4edc1e2b9f20d
Léonard Verschuere <leonard.verschuere@epitech.eu>
2023-06-07 11:26:08 +0200

    [ADD] unkown command are now handled and some malloc & free have been removed from the command 'plv' 'pin' 'ppo'

--- server/src/server/retrieve_command.c
+++ server/src/server/retrieve_command.c
@@ -55,6 +55,7 @@ int receive_client_msg(int sockfd, fd_set *readfds, serv_t *serv)
     } else {
         cmd = parse_command(buffer);
         if (cmd == -1) {
+            write(sockfd, "suc\n", 4);
             next = check_name_team(serv, buffer);
             if (next > 0) {
                 fill_client_struct(sockfd, serv, buffer);
