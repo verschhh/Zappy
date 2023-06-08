52a0c23b61018bd6ed0facfa561f9b3222740987
Léonard Verschuere <leonard.verschuere@epitech.eu>
2023-06-08 18:02:11 +0200

    [ADD] end of game function to end the game

--- server/src/command/player.c
+++ server/src/command/player.c
@@ -120,3 +120,41 @@ int send_broadcast(int sockfd, serv_t *serv, char *buffer)
     write(serv->sockfd, send, len);
     return 0;
 }
+
+int check_level(slot_t *tmp)
+{
+    int count = 0;
+    while (tmp->team_member->next != NULL) {
+        if (tmp->team_member->player->level == 8)
+            count++;
+        tmp->team_member = tmp->team_member->next;
+       if (count >= 6)
+            return 1;
+    }
+    return 0;
+}
+
+int check_end_game(serv_t *serv)
+{
+    int count = 0;
+    slot_t *slot = serv->slots;
+    while (slot->next != NULL) {
+        if (check_level(slot) == 1)
+            return 1;
+    }
+    return 0;
+}
+
+int end_of_game(int sockfd, serv_t *serv, char *buffer)
+{
+    (void)buffer;
+    char msg[10];
+    int end = 0;
+    update_teams(serv);
+    end = check_end_game(serv);
+    if (end == 0)
+        return 0;
+    sprintf(msg, "seg %s\n", serv->clients->team_name);
+    write(serv->sockfd, msg, 10);
+    exit (0);
+}
