d03b221ce95fa7010738cd44f9dbe53ddb61cc53
Léonard Verschuere <leonard.verschuere@epitech.eu>
2023-06-15 11:09:54 +0200

    [FIX] crash in compilation

--- server/src/ai_command/move.c
+++ server/src/ai_command/move.c
@@ -7,23 +7,23 @@
 
 #include "../../includes/zappy.h"
 
-int forward(int sockfd, serv_t *serv, char *buffer)
-{
+// int forward(int sockfd, serv_t *serv, char *buffer)
+// {
     
-}
+// }
 
-int inventory(int sockfd, serv_t *serv, char *buffer)
-{
-    client_t *client = get_correct_client(serv, sockfd);
-    char *msg = NULL;
-    int len = snprintf(msg, 0, "[food %d, linemate %d, deraumere %d, sibur %d,
-        mendiane %d, phiras %d, thystame %d]\n", tmp->food, tmp->linemate,
-        tmp->deraumere, tmp->sibur, tmp->mendiane, tmp->phiras, tmp->thystame);
-    msg = malloc(sizeof(char) * len + 1);
-    msg[len] = '\0';
-    snprintf(msg, len, "[food %d, linemate %d, deraumere %d, sibur %d,
-        mendiane %d, phiras %d, thystame %d]\n", tmp->food, tmp->linemate,
-        tmp->deraumere, tmp->sibur, tmp->mendiane, tmp->phiras, tmp->thystame);
-    write(sockfd, msg, len);
-    return 0;
-}
+// int inventory(int sockfd, serv_t *serv, char *buffer)
+// {
+//     client_t *client = get_correct_client(serv, sockfd);
+//     char *msg = NULL;
+//     int len = snprintf(msg, 0, "[food %d, linemate %d, deraumere %d, sibur %d,
+//         mendiane %d, phiras %d, thystame %d]\n", tmp->food, tmp->linemate,
+//         tmp->deraumere, tmp->sibur, tmp->mendiane, tmp->phiras, tmp->thystame);
+//     msg = malloc(sizeof(char) * len + 1);
+//     msg[len] = '\0';
+//     snprintf(msg, len, "[food %d, linemate %d, deraumere %d, sibur %d,
+//         mendiane %d, phiras %d, thystame %d]\n", tmp->food, tmp->linemate,
+//         tmp->deraumere, tmp->sibur, tmp->mendiane, tmp->phiras, tmp->thystame);
+//     write(sockfd, msg, len);
+//     return 0;
+// }
