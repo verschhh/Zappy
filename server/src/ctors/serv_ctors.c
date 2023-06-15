dfa7965ca6dcfcc962098043191864d387081804
Léonard Verschuere <leonard.verschuere@epitech.eu>
2023-06-15 10:15:14 +0200

    [ADD] basic clock to implement the time

--- server/src/ctors/serv_ctors.c
+++ server/src/ctors/serv_ctors.c
@@ -30,7 +30,6 @@ void serv_ctor2(serv_t *serv, args_t *arg)
     serv->slots = slot_ctor(arg);
     for (int i = 1; arg->names[i] != NULL; i++)
         new_slot(serv->slots, arg->names[i]);
-    serv->start = clock();
     return serv;
 }
 
