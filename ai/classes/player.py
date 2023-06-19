67d8fdd919e902219746227e5f76d0f3e8c95816
durbaon <nathanpaillet@gmail.com>
2023-06-19 18:31:06 +0200

    [ADD] evolve to lvl3 with 2 players on map

--- ai/classes/player.py
+++ ai/classes/player.py
@@ -6,10 +6,20 @@
 ##
 
 from math import ceil
-
+from random import randint
+from time import sleep
 
 SUCESS = 0
 FAIL = 84
+
+class Incantation:
+    LVL_1 = {"player": 1, "linemate": 1, "deraumere": 0, "sibur": 0, "mendiane": 0, "phiras": 0, "thystame": 0}
+    LVL_2 = {"player": 2, "linemate": 1, "deraumere": 1, "sibur": 1, "phiras": 0, "thystame": 0, "mendiane": 0}
+    LVL_3 = {"player": 2, "linemate": 2, "sibur": 1, "phiras": 2, "thystame": 0, "mendiane": 0, "deraumere": 0}
+    LVL_4 = {"player": 4, "linemate": 1, "deraumere": 1, "sibur": 2, "phiras": 1, "thystame": 0, "mendiane": 0}
+    LVL_5 = {"player": 4, "linemate": 1, "deraumere": 2, "sibur": 1, "mendiane": 3, "phiras": 0, "thystame": 0}
+    LVL_6 = {"player": 6, "linemate": 1, "deraumere": 2, "sibur": 3, "phiras": 1, "thystame": 0, "mendiane": 0}
+    LVL_7 = {"player": 6, "linemate": 2, "deraumere": 2, "sibur": 2, "mendiane": 2, "phiras": 2, "thystame": 1}
 
 class Orientation:
     NORTH = 0
@@ -28,11 +38,11 @@ class Priority:
     THYSTAME = 7
 
 class Player:
-    def __init__(self, socket, game):
+    def __init__(self, socket, map_size_x, map_size_y):
         self.x = 5
         self.y = 5
-        self.max_x = game.map_size_x - 1
-        self.max_y = game.map_size_y - 1
+        self.max_x = map_size_x - 1
+        self.max_y = map_size_y - 1
         self.level = 1
         self._inventory = {"food": 10, "linemate": 0, "deraumere": 0, "sibur": 0, "mendiane": 0, "phiras": 0, "thystame": 0}
         self.team = ""
@@ -41,6 +51,51 @@ class Player:
         self.socket = socket
         self.vision = []
         self.vision_with_pos = []
+        self.inv_other_player = []
+        self.id = randint(0, 10000000)
+        self.nb_joueur_ready = 0
+        self.incantation_direction = 0
+        self.incantation_launched = False
+
+    def player_is_in_inventory(self, id):
+        print(id)
+        for i in self.inv_other_player:
+            if i["id"] == int(id):
+                print("True")
+                return True
+        print("False")
+        return False
+
+    def drop_all_ressources(self):
+        for ressource in self._inventory:
+            print(ressource)
+            tmp = 0
+            while self._inventory[ressource] > 0 and ressource != "food" and tmp <= 4:
+                print(f"ressource = {ressource}")
+                self.set(ressource)
+                tmp += 1
+
+    def update_inventory_other_player(self, message):
+        print(message)
+        message = message.split(' ')
+        print(message[-1].split('\n'))
+        message[-1] = message[-1].split('\n')[0]
+        if self.player_is_in_inventory(message[0]) == False:
+            self.inv_other_player.append({"id": int(message[0]), "inventory": {"food": 0, "linemate": 0, "deraumere": 0, "sibur": 0, "mendiane": 0, "phiras": 0, "thystame": 0}})
+
+        for i in self.inv_other_player:
+            if i["id"] == int(message[0]):
+                i["inventory"]["food"] = int(message[1])
+                i["inventory"]["linemate"] = int(message[2])
+                i["inventory"]["deraumere"] = int(message[3])
+                i["inventory"]["sibur"] = int(message[4])
+                i["inventory"]["mendiane"] = int(message[5])
+                i["inventory"]["phiras"] = int(message[6])
+                if message[7][-1] == '\n' or message[7][-1] == '[':
+                    message[7] = message[7][:-1]
+                    if message[7][-1] == '[' or message[7][-1] == '\n':
+                        message[7] = message[7][:-1]
+                i["inventory"]["thystame"] = int(message[7])
 
     def get_distance(self, x, y):
         dx = abs(self.x - x)
@@ -53,7 +108,7 @@ class Player:
 
     def forward(self):
         self.socket.send("Forward")
-        self.socket.receive()
+        self.socket.receive(self)
         if self.orientation == Orientation.NORTH:
             self.y -= 1
         elif self.orientation == Orientation.EAST:
@@ -73,24 +128,28 @@ class Player:
 
     def right(self):
         self.socket.send("Right")
-        self.socket.receive()
+        self.socket.receive(self)
         self.orientation += 1
         if self.orientation > Orientation.WEST:
             self.orientation = Orientation.NORTH
 
     def left(self):
         self.socket.send("Left")
-        self.socket.receive()
+        self.socket.receive(self)
         self.orientation -= 1
         if self.orientation < Orientation.NORTH:
             self.orientation = Orientation.WEST
 
     def look(self):
         self.socket.send("Look")
-        self.socket.receive()
+        self.socket.receive(self)
+        # print(self.socket.buffer)
+        while self.socket.buffer == "ko\n" or self.socket.buffer[-1] != "\n":
+            self.socket.send("Look")
+            self.socket.receive(self)
+
         buff = self.socket.buffer[2:-2]
         i = 0
-        # print(buff)
         while buff[i] != '\0':
             if buff[i] == ',' and buff[i + 1] == ' ':
                 buff = buff[:i + 1] + buff[i + 2:]
@@ -166,7 +225,7 @@ class Player:
 
     def eject(self):
         self.socket.send("Eject")
-        self.socket.receive()
+        self.socket.receive(self)
         buff = self.socket.buffer
         if buff == "ok":
             return SUCESS
@@ -174,63 +233,189 @@ class Player:
 
     def fork(self):
         self.socket.send("Fork")
-        self.socket.receive()
+        self.socket.receive(self)
 
     def connect_nbr(self):
         self.socket.send("Connect_nbr")
-        self.socket.receive()
+        self.socket.receive(self)
         return int(self.socket.buffer)
 
+    def send_inventory(self):
+        message = "Inventory,"
+        message += str(self.id) + " "
+        for key, value in self._inventory.items():
+            message += f"{value}"
+            if key != "thystame":
+                message += " "
+        return message
+
     def broadcast(self, message):
+        print(f"id{self.id} send this message : {message}")
         self.socket.send(f"Broadcast {message}")
-        self.socket.receive()
+        self.socket.receive(self)
+        if message == "Ready":
+            self.socket.receive(self)
 
     def inventory(self):
         self.socket.send("Inventory")
-        self.socket.receive()
-        print(f"Inventory: {self.socket.buffer}")
-        print(f"My inventory: {self._inventory}")
+        self.socket.receive(self)
         return self._inventory
 
     def take(self, item):
         self.socket.send(f"Take {item}")
-        self.socket.receive()
+        self.socket.receive(self)
         if self.socket.buffer == "ok\n":
             self._inventory[item] += 1
-            print("Success")
             return SUCESS
-        print("Fail")
         return FAIL
 
     def set(self, item):
         self.socket.send(f"Set {item}")
-        self.socket.receive()
+        self.socket.receive(self)
         if self.socket.buffer == "ok\n":
             self._inventory[item] -= 1
-            print("Success")
             return SUCESS
-        print("Fail")
         return FAIL
 
     def incantation(self):
         self.socket.send("Incantation")
-        self.socket.receive()
+        self.socket.receive(self)
         buff = self.socket.buffer
         print(buff)
         if buff == "ko\n":
             print("Incantation Failed")
             self.socket.buffer = ""
             print("Wait to feel better")
-            self.socket.receive()
+            self.socket.receive(self)
             print("END")
             return FAIL
         print("Elevation Started\n")
         self.socket.buffer = ""
-        self.socket.receive()
+        self.socket.receive(self)
         if self.socket.buffer.split(' ')[0] == "Current":
-            self.level += 1
+
             print("Elevation Finished")
             print("You are now level %d" % (self.level))
             return SUCESS
         print("Elevation Failed")
+        print(self.look()[0])
         return FAIL
+
+    def go_to_direction(self):
+        print(f"self.incantation_direction = {self.incantation_direction}")
+        while int(self.incantation_direction) != 0:
+            print(int(self.incantation_direction))
+            match int(self.incantation_direction):
+                case 0:
+                    print("I am here")
+                    break
+                case 1:
+                    self.forward()
+                    print("Front")
+
+                case 2:
+                    self.forward()
+                    self.left()
+                    self.forward()
+                    self.left()
+                    print("Front Left")
+
+                case 3:
+                    self.left()
+                    self.forward()
+                    print("Left")
+
+                case 4:
+                    self.left()
+                    self.forward()
+                    self.left()
+                    self.forward()
+                    print("Back Left")
+
+                case 5:
+                    self.left()
+                    self.left()
+                    self.forward()
+                    print("Back")
+
+                case 6:
+                    self.right()
+                    self.forward()
+                    self.right()
+                    self.forward()
+                    print("Back Right")
+
+                case 7:
+                    self.right()
+                    self.forward()
+                    print("Right")
+
+                case 8:
+                    self.right()
+                    self.forward()
+                    self.right()
+                    self.forward()
+                    print("Front Right")
+
+        self.drop_all_ressources()
+        match int(self.incantation_direction):
+                case 0:
+                    print("I am here")
+                case 1:
+                    self.forward()
+                    print("Front")
+
+                case 2:
+                    self.forward()
+                    self.left()
+                    self.forward()
+                    self.right()
+                    print("Front Left")
+
+                case 3:
+                    self.left()
+                    self.forward()
+                    print("Left")
+
+                case 4:
+                    self.left()
+                    self.forward()
+                    self.left()
+                    self.forward()
+                    self.right()
+                    print("Back Left")
+
+                case 5:
+                    self.left()
+                    self.left()
+                    self.forward()
+                    print("Back")
+
+                case 6:
+                    self.right()
+                    self.forward()
+                    self.right()
+                    self.forward()
+                    print("Back Right")
+
+                case 7:
+                    self.right()
+                    self.forward()
+                    print("Right")
+
+                case 8:
+                    self.right()
+                    self.forward()
+                    self.left()
+                    self.forward()
+                    print("Front Right")
+
+        self.broadcast("Ready")
+        print(self.look()[0])
+
+    def call_for_incantation(self, direction, message):
+        for i in message:
+            print(f"i = {i}, self.id = {self.id}")
+            if int(i) == self.id:
+                self.incantation_direction = direction
+                self.go_to_direction()
--- ai/classes/socket.py
+++ ai/classes/socket.py
@@ -27,8 +27,9 @@ class Socket:
         self._sock.sendall((message+"\n").encode())
         self.buffer = ""
 
-    def receive(self):
+    def receive(self, player):
         buff = self._sock.recv(4096)
+        print(f"buff = {buff}")
         if not buff:
             print("Server disconnected")
             sys.exit(10)
@@ -36,13 +37,39 @@ class Socket:
             print("You died")
             sys.exit(1)
         elif buff.decode().split(' ')[0] == "message":
-            print("Message from %s: %s" % (buff.decode().split(' ')[1], buff.decode().split(' ')[2]))
+
+            buff = buff.decode().split(',')
+            # print(f"Message received {buff}")
+            if player.level > 1:
+                print(buff)
+                if buff[1] == " Inventory":
+                    print("Updating inventory")
+                    print(buff[2][:-1])
+                    buff[2] = buff[2][:-1]
+                    if buff[2][-1] != '\n':
+                        print("Rennntree")
+                        player.update_inventory_other_player(buff[2])
+                    print("Updated inventory")
+                elif buff[1] == " Ready\n":
+                    player.nb_joueur_ready += 1
+                elif buff[1] == " Incantation":
+                    print("Incantation launched")
+                    print(f"buff[0].split(' ')[1] = {buff[0].split(' ')[1]}, buff[2] = {buff[2].split(' ')}")
+                    player.call_for_incantation(buff[0].split(" ")[1], buff[2].split(' ')[:-1])
+                    print("Incantation finished")
+                elif buff[1] == " Here\n":
+                    print(f"New direction ;{buff[0].split(' ')[1]};")
+                    player.incantation_direction = int(buff[0].split(' ')[1])
             self.buffer = ""
-            self.receive()
-        elif buff.decode().split(':')[0] == "eject":
-            print
+            self.receive(player)
         else:
             self.buffer += buff.decode()
+            if self.buffer.split(' ')[0] == "Current":
+                player.level += 1
+                print(player.level)
+            if player.level == 3:
+                exit(333)
+            
 
     def close(self):
         self._sock.close()
