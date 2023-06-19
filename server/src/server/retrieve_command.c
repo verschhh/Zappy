e75ffc317a7d0ed6bbfe60ccc62e1d1b4e5a822b
Dimerci <maxime.lambert@ngine.co>
2023-06-19 15:27:27 +0200

    [ADD] if slot is full no player can be created + incantation function

--- server/src/server/retrieve_command.c
+++ server/src/server/retrieve_command.c
@@ -78,7 +78,7 @@ int receive_client_msg(int sockfd, fd_set *readfds, serv_t *serv)
         cmd = parse_command(buffer);
         if (cmd == -1) {
             next = check_name_team(serv, buffer);
-            if (next > 0) {
+            if (next >= 0) {
                 fill_client_struct(sockfd, serv, buffer);
                 send_x_y_ai(sockfd, serv, next);
                 send_connection_msg(serv->clients, serv);
