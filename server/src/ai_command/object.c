33690ff556c307cd72802a4861930130ddb69d7f
Dimerci <maxime.lambert@ngine.co>
2023-06-19 16:36:24 +0200

    [FIX] rename file

---
+++ server/src/ai_command/object.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2022
+** Zappy
+** File description:
+** object.c
+*/
+
+#include "../../includes/zappy.h"
+
+int take_object(int sockfd, serv_t *serv, char *buffer)
+{
+    
+}
