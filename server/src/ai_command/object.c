44cb93eaa20cea38e1a204b01794bb26f3282129
Dimerci <maxime.lambert@ngine.co>
2023-06-20 10:26:18 +0200

    [MERGE]

--- server/src/ai_command/object.c
+++ server/src/ai_command/object.c
@@ -14,16 +14,22 @@ int check_element_tile(map_t *map, MapFieldFunc getFieldFunc)
     else return 0;
 }
 
+void get_correct_tile(map_t *map, client_t *client)
+{
+
+}
+
 int take_object(int sockfd, serv_t *serv, char *buffer)
 {
     map_t *map_cpy = serv->map;
     int count = 0;
     char **array = split_string_at_spaces(buffer, &count);
-    char *food[7] = {"food", "linemate\n", "deraumere\n",
-        "sibur\n", "mendiane\n", "phiras\n", "thystame\n"};
+    char *food[7] = {"food", "linemate", "deraumere",
+        "sibur", "mendiane", "phiras", "thystame"};
     MapFieldFunc fieldFuncs[7] = {FOOD(serv->map), LINEMATE(serv->map), DERAUMERE(serv->map),
         SIBUR(serv->map), MENDIANE(serv->map), PHIRAS(serv->map), THYSTAME(serv->map)};
 
+    get_correct_client(map_cpy, get_correct_client(serv, sockfd));
     for (int i = 0; i != 7; i++) {
         if (strstr(array[1], food[i]) != NULL) {
             check_element_tile(map_cpy, fieldFuncs[i]);
