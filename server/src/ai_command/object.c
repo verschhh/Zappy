d0b369f814d611e07337911c1e1162d21413e11f
Dimerci <maxime.lambert@ngine.co>
2023-06-20 16:30:52 +0200

    [ADD] ko message of object doesnt exist on a tile when take command is received

--- server/src/ai_command/object.c
+++ server/src/ai_command/object.c
@@ -7,12 +7,16 @@
 
 #include "../../includes/zappy.h"
 
-int check_element_tile(map_t *map, int materials)
+int check_element_tile(map_t *map, int materials, int sockfd)
 {
-    if (materials > 0)
+    if (materials > 0) {
+        write(sockfd, "ok\n", 4);
         return 1;
-    else
+    }
+    else {
+        write(sockfd, "ko\n", 4);
         return 0;
+    }
 }
 
 map_t *get_correct_tile(map_t *map, client_t *client)
@@ -26,52 +30,52 @@ map_t *get_correct_tile(map_t *map, client_t *client)
     return NULL;
 }
 
-void switch_case_take_two(int i, map_t *map_cpy, client_t *client_cpy)
+void switch_case_take_two(int i, map_t *map_cpy, client_t *client_cpy, int sockfd)
 {
     switch (i) {
         case 3:
-            if (check_element_tile(map_cpy, map_cpy->sibur) == 1)
+            if (check_element_tile(map_cpy, map_cpy->sibur, sockfd) == 1)
                 map_cpy->sibur--;
                 client_cpy->player->inventory->sibur++;
             break;
         case 4:
-            if (check_element_tile(map_cpy, map_cpy->mendiane) == 1)
+            if (check_element_tile(map_cpy, map_cpy->mendiane, sockfd) == 1)
                 map_cpy->mendiane--;
                 client_cpy->player->inventory->mendiane++;
             break;
         case 5:
-            if (check_element_tile(map_cpy, map_cpy->phiras) == 1)
+            if (check_element_tile(map_cpy, map_cpy->phiras, sockfd) == 1)
                 map_cpy->phiras--;
                 client_cpy->player->inventory->phiras++;
             break;
         case 6:
-            if (check_element_tile(map_cpy, map_cpy->thystame) == 1)
+            if (check_element_tile(map_cpy, map_cpy->thystame, sockfd) == 1)
                 map_cpy->thystame--;
                 client_cpy->player->inventory->thystame++;
             break;
     }
 }
 
-void switch_case_take(int i, map_t *map_cpy, client_t *client_cpy)
+void switch_case_take(int i, map_t *map_cpy, client_t *client_cpy, int sockfd)
 {
     switch (i) {
         case 0:
-            if (check_element_tile(map_cpy, map_cpy->food) == 1)
+            if (check_element_tile(map_cpy, map_cpy->food, sockfd) == 1)
                 map_cpy->food--;
                 client_cpy->player->inventory->food++;
             break;
         case 1:
-            if (check_element_tile(map_cpy, map_cpy->linemate) == 1)
+            if (check_element_tile(map_cpy, map_cpy->linemate, sockfd) == 1)
                 map_cpy->linemate--;
                 client_cpy->player->inventory->linemate++;
             break;
         case 2:
-            if (check_element_tile(map_cpy, map_cpy->deraumere) == 1)
+            if (check_element_tile(map_cpy, map_cpy->deraumere, sockfd) == 1)
                 map_cpy->deraumere--;
                 client_cpy->player->inventory->deraumere++;
             break;
         default:
-            switch_case_take_two(i, map_cpy, client_cpy);
+            switch_case_take_two(i, map_cpy, client_cpy, sockfd);
             break;
     }
 }
@@ -89,9 +93,8 @@ int take_object(int sockfd, serv_t *serv, char *buffer)
     printf("Position = %d %d\n", map_cpy->x, map_cpy->y);
     for (int i = 0; i < 7; i++) {
         if (strstr(array[1], food[i]) != NULL) {
-            switch_case_take(i, map_cpy, client_cpy);
+            switch_case_take(i, map_cpy, client_cpy, sockfd);
         }
     }
-    write(sockfd, "ok\n", 4);
     return 0;
 }
