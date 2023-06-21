9b5374376718b696051c6a9863edd2036ee62c6e
Léonard Verschuere <leonard.verschuere@epitech.eu>
2023-06-21 14:11:56 +0200

    [FIX] warnings

--- server/includes/zappy.h
+++ server/includes/zappy.h
@@ -53,6 +53,7 @@ map_t *find_tile(serv_t *server, int x, int y);
 void update_time_limit(serv_t *serv, client_t *client, int time_limit, char *s);
 uint64_t micro_time(void);
 map_t *get_correct_tile(map_t *map, client_t *client);
+int check_element_tile(int materials, int sockfd);
 
 //* Commands Gui
 int map_size(int sockfd, serv_t *serv, char *buffer);
--- server/src/ai_command/take.c
+++ server/src/ai_command/take.c
@@ -34,27 +34,27 @@ void switch_case_take_two(int i, map_t *map_cpy, client_t *client_cpy, int sockf
 {
     switch (i) {
         case 3:
-            if (check_element_tile(map_cpy->sibur, sockfd) == 1) {
+            if (check_element_tile(map_cpy->sibur, sockfd) == 1) {// check if there is enough sibur / warning
                 map_cpy->sibur--;
-                client_cpy->player->inventory->sibur++;
+                client_cpy->player->inventory->sibur++; //warning
             }
             break;
         case 4:
-            if (check_element_tile(map_cpy->mendiane, sockfd) == 1) {
+            if (check_element_tile(map_cpy->mendiane, sockfd) == 1) {// check if there is enough mendiane / warning
                 map_cpy->mendiane--;
-                client_cpy->player->inventory->mendiane++;
+                client_cpy->player->inventory->mendiane++; //warning
             }
             break;
         case 5:
-            if (check_element_tile(map_cpy->phiras, sockfd) == 1) {
+            if (check_element_tile(map_cpy->phiras, sockfd) == 1) {// check if there is enough phiras / warning
                 map_cpy->phiras--;
-                client_cpy->player->inventory->phiras++;
+                client_cpy->player->inventory->phiras++; //warning
             }
             break;
         case 6:
-            if (check_element_tile(map_cpy->thystame, sockfd) == 1) {
+            if (check_element_tile(map_cpy->thystame, sockfd) == 1) {// check if there is enough thystame / warning
                 map_cpy->thystame--;
-                client_cpy->player->inventory->thystame++;
+                client_cpy->player->inventory->thystame++; //warning
             }
             break;
     }
@@ -64,21 +64,21 @@ void switch_case_take(int i, map_t *map_cpy, client_t *client_cpy, int sockfd)
 {
     switch (i) {
         case 0:
-            if (check_element_tile(map_cpy->food, sockfd) == 1) {
+            if (check_element_tile(map_cpy->food, sockfd) == 1) {// check if there is enough food / warning
                 map_cpy->food--;
-                client_cpy->player->inventory->food++;
+                client_cpy->player->inventory->food++; //warning
             }
             break;
         case 1:
-            if (check_element_tile(map_cpy->linemate, sockfd) == 1) {
+            if (check_element_tile(map_cpy->linemate, sockfd) == 1) {// check if there is enough linemate / warning
                 map_cpy->linemate--;
-                client_cpy->player->inventory->linemate++;
+                client_cpy->player->inventory->linemate++; //warning
             }
             break;
         case 2:
-            if (check_element_tile(map_cpy->deraumere, sockfd) == 1) {
+            if (check_element_tile(map_cpy->deraumere, sockfd) == 1) {// check if there is enough deraumere / warning
                 map_cpy->deraumere--;
-                client_cpy->player->inventory->deraumere++;
+                client_cpy->player->inventory->deraumere++; //warning
             }
             break;
         default:
