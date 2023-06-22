daa4c307138898bd459fe0540d27ce66234df7d7
Léonard Verschuere <leonard.verschuere@epitech.eu>
2023-06-22 12:20:17 +0200

    [FIX] solved some valgrind in tile_to_string function because malloc was not really malleable

--- server/src/tools_two.c
+++ server/src/tools_two.c
@@ -19,9 +19,27 @@ map_t *find_tile(serv_t *server, int x, int y)
     return NULL;
 }
 
-int my_modulo(double nb, double div)
+int calc_tile_to_string(map_t *tile, client_t *client)
 {
-    int result = nb / div;
-    
-
+    int len = 0;
+    while (client != NULL) {
+        if (client->player->x == tile->x && client->player->y == tile->y)
+            len += 8;
+        client = client->next;
+    }
+    for (int i = 0; i < tile->food; i++)
+        len += 6;
+    for (int i = 0; i < tile->linemate; i++)
+        len += 10;
+    for (int i = 0; i < tile->deraumere; i++)
+        len += 11;
+    for (int i = 0; i < tile->sibur; i++)
+        len += 7;
+    for (int i = 0; i < tile->mendiane; i++)
+        len += 10;
+    for (int i = 0; i < tile->phiras; i++)
+        len += 8;
+    for (int i = 0; i < tile->thystame; i++)
+        len += 10;
+    return len;
 }
