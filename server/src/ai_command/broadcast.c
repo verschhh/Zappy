a08a1cbf888cd2f6f42f411c462998dc3f65064d
durbaon <nathanpaillet@gmail.com>
2023-06-24 13:12:51 +0200

    [FIX] Broadcast orientation

--- server/src/ai_command/broadcast.c
+++ server/src/ai_command/broadcast.c
@@ -13,7 +13,7 @@ int sound(player_t *start, player_t *goal)
    if (orientation == 0)
        return 0;
     if (goal->orientation == EAST)
-        orientation += 8;
+        orientation += 6;
     if (goal->orientation == SOUTH)
         orientation += 4;
     if (goal->orientation == WEST)
@@ -26,35 +26,32 @@ int sound(player_t *start, player_t *goal)
 void print_broadcast(client_t *client, client_t *actual_client, int sockfd, char *buffer)
 {
     char msg[0];
-    int len = snprintf(msg, 0, "message %d,%s",
-        sound(actual_client->player, client->player), buffer);
+    int len = snprintf(msg, 0, "message 8,%s", buffer);
     char send[len];
-    for (int i = 0; client != NULL; i++) {
+    for (; client != NULL; client = client->next) {
         if (client->sockfd != sockfd) {
             sprintf(send, "message %d,%s\n",
                 sound(actual_client->player, client->player), buffer);
-            write(client->sockfd, send, len - 1);
-            break;
+            printf(send);
+            write(client->sockfd, send, len);
         }
-        client = client->next;
     }
     write(sockfd, "ok\n", 3);
 }
 
 int broadcast(int sockfd, serv_t *serv, char *buffer)
 {
-    client_t *client = serv->clients;
+    client_t *client;
     client_t *actual_client = get_correct_client(serv, sockfd);
 
     if (!actual_client->clocking) {
-        update_time_limit(serv, client, 7, buffer);
+        update_time_limit(serv, actual_client, 7, buffer);
         return 0;
     }
     if (actual_client->clocking) {
         char msg[0];
         buffer += 9;
-        int len = snprintf(msg, 0, "message %d,%s",
-            sound(actual_client->player, client->player), buffer);
+        client = serv->clients;
         print_broadcast(client, actual_client, sockfd, buffer);
         actual_client->clocking = false;
     }
